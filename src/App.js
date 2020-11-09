7f7a88c40e30ed2e6c14c7521288bcd7a8359e7a
arthur langenberg <arthurlangenberg@dtsidev.com>
2020-11-09 11:25:51 -0600

    added candidates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React from "react";
 
-export const App  = () =>{
-    return <h2>My component</h2>
-}
+export const App = () => {
+  const candidates = [
+    { name: "Bunny, Bugs", rank: "Airman" },
+    { name: "Gonzales, Speedy", rank: "General" },
+    { name: "Fudd, Elmer", rank: "Tech Seargent" },
+  ];
+  function renderCandidate(candidate) {
+    return (
+      <li>
+        {candidate.name} - {candidate.rank}
+      </li>
+    );
+  }
+  return (
+    <React.Fragment>
+      <h2>Candidates</h2>
+      <ul>{candidates.map(renderCandidate)}</ul>
+    </React.Fragment>
+  );
+};
