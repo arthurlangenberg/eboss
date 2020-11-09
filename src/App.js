f7297ea32edbc4577ff692b85abd13e626a7c0f5
arthur langenberg <arthurlangenberg@dtsidev.com>
2020-11-09 14:44:51 -0600

    creating routes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import React from "react";
-import { getCandidates } from "./api/candidatesApi";
+import { Candidates } from "./Candidates";
+import { CandidateDetails } from "./CandidateDetails";
+import { Route } from "react-router-dom";
+export function App() {
+  return (
+    <>
+      <Route path="/">
+        <Candidates />
+      </Route>
 
-export class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      candidates: [],
-    };
-  }
-
-  async componentDidMount() {
-    const candidates = await getCandidates();
-    this.setState({ candidates: candidates });
-  }
-  renderCandidate(candidate) {
-    return (
-      <tr key={candidate.id}>
-        <td>{candidate.name}</td>
-        <td>{candidate.rank}</td>
-      </tr>
-    );
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <h2>Candidates</h2>
-        <table border="1">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Rank</th>
-            </tr>
-          </thead>
-          <tbody>{this.state.candidates.map(this.renderCandidate)}</tbody>
-        </table>
-      </React.Fragment>
-    );
-  }
+      <Route path="/details">
+        <CandidateDetails />
+      </Route>
+    </>
+  );
 }
---
+++ src/Candidates.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { getCandidates } from "./api/candidatesApi";
+
+export class Candidates extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      candidates: [],
+    };
+  }
+
+  async componentDidMount() {
+    const candidates = await getCandidates();
+    this.setState({ candidates: candidates });
+  }
+  renderCandidate(candidate) {
+    return (
+      <tr key={candidate.id}>
+        <td>{candidate.name}</td>
+        <td>{candidate.rank}</td>
+      </tr>
+    );
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Candidates</h2>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Rank</th>
+            </tr>
+          </thead>
+          <tbody>{this.state.candidates.map(this.renderCandidate)}</tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
